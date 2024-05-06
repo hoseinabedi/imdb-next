00c5a1881615cfab635b2302f2ad34e4fa7102fc
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-05-07 00:04:54 +0330

    Add & Set Header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "IMDB Movies",
-  description: "This is the IMDB Movies",
+    title: "IMDB Movies",
+    description: "This is the IMDB Movies",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
