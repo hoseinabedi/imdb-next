7a5e62f98f41d79c9c0032ba1ad2ed1a1ccf4d1c
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-05-07 23:59:17 +0330

    Improve Header & MenuItem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { React } from 'react';
 import MenuItem from './MenuItem';
+import { AiFillHome } from'react-icons/ai';
+import { BsFillInfoCircleFill} from 'react-icons/bs';
 
 const Header = () => {
     return(
         <div>
             <div className=''>
-                <MenuItem />
+                <MenuItem title="home" address='/' Icon={AiFillHome} />
+                <MenuItem title="about" address='/about' Icon={BsFillInfoCircleFill} />
             </div>
         </div>
     )
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
-export default function MenuItem() {
+export default function MenuItem({ title, address, Icon }) {
     return (
-        <div>MenuItem</div>
+        <Link href={address} className='hover:text-amber-500'>
+            <Icon className='text-2xl sm:hidden' />
+            <p className='uppercase hidden sm:inline text-sm'>{title}</p>
+        </Link>
     )
 }
