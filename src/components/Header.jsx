692a68d78d7883a7ad445a925e0e146e6cc95319
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-05-09 01:01:58 +0330

    Add some styles for header menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,14 @@ import { BsFillInfoCircleFill} from 'react-icons/bs';
 
 const Header = () => {
     return(
-        <div>
-            <div className=''>
+        <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+            <div className='flex gap-4'>
                 <MenuItem title="home" address='/' Icon={AiFillHome} />
                 <MenuItem title="about" address='/about' Icon={BsFillInfoCircleFill} />
+            </div>
+            <div className='flex gap-1 items-center'>
+                <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
+                <span className='text-xl hidden sm:inline'>Clone</span>
             </div>
         </div>
     )
