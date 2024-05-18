0f8444496dccffb9c04c1cf5f0a12202cbc5df25
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-05-18 23:44:02 +0330

    Add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className='max-w-6xl mx-auto p-3 space-y-4'>
+            <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt lorem metus, in lacinia dui sodales viverra. Sed iaculis est mauris, a varius eros fringilla a. Nunc sollicitudin eu arcu gravida viverra. Nulla sed magna euismod, suscipit risus eu, mollis nunc. Ut vel enim ac purus luctus facilisis. Nulla vel convallis diam. Praesent gravida at nunc sed fermentum.</p>
+            <p>Suspendisse lobortis, turpis sed viverra posuere, purus risus lacinia mauris, et faucibus quam magna id tortor. Praesent egestas augue eros, in commodo magna iaculis a. Mauris maximus quam eu neque cursus interdum. Donec volutpat ac risus a convallis. Ut quis auctor metus. Quisque quam elit, vehicula vitae dictum eu, vestibulum sed sem. Ut suscipit nunc ipsum, ac imperdiet odio rhoncus vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus varius, turpis eu pellentesque faucibus, arcu risus rutrum est, at venenatis ipsum nulla vel tellus. Suspendisse ac urna ac urna aliquam fringilla nec quis dolor. Aenean pulvinar faucibus ipsum sed tempus.</p>
+        </div>
+    )
+}
