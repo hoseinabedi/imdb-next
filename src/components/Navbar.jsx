4e0f1604793ad135c773d7e7ba9e214a3501ab62
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-05-22 23:34:47 +0330

    Improve Navbar UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="flex dark:bg-gray-600 bg-amber-100 gap-6 p-4 lg:text-lg justify-center">
             <NavbarItem title="Trending" param="fetchTrending" />
             <NavbarItem title="Top Rated" param="fetchTopRated" />
         </div>
